607197af5ae725db49d277bc94c6c309724107bb
Shreyash Neeraj <shreyashn.dev@gmail.com>
2024-11-15 09:39:23 +0530

    push

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,6 @@ import java.util.List;
 
 public class Main {
     public void main(){
-        int[] nums = {0,0,1,1,1,2,2,3,3,3};
-        removeDuplicates(nums);
-        Print.printArray(nums);
+
     }
 }
